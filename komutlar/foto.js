c236067c3a41baeff24cc512a3468b0d0ab2a2cd
Utku Ömer Kılıç <omerleroyunda@gmail.com>
2021-01-20 13:31:27 +0300

    komut değişikliği

---
+++ komutlar/foto.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+let bot_version = process.env.VERSION;
+let bot_sahip = process.env.SAHIP;
+const moment = require("moment");
+const cheerio = require("cheerio");
+const os = require("os");
+const request = require("request");
+require("moment-duration-format");
+
+exports.run = async (client, message, args) => {
+  const msg = new Discord.MessageEmbed()
+    get('https://aws.random.cat/meow').then(response => {
+      message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[2]}`}]});
+      console.log('random cat picture');
+      })
+      } catch (e) {
+            console.log('error!');
+            }
+          };
+  return message.channel.send(msg);
+};
+
+exports.help = {
+  name: "foto",
+  guildOnly: false, // false olur ise DM'den de kullanılabilir bi hal alır | true olur ise sadece sunucuda kullanılabilir bir halde olur
+  aliases: ["i"]
+};
+
+
+
+

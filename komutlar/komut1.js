709b8ca8234f28c7c61cb436d16e20d109135631
Utku Ömer Kılıç <omerleroyunda@gmail.com>
2021-01-20 11:43:39 +0300

    Create komut1.js

---
+++ komutlar/komut1.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+let bot_version = process.env.VERSION;
+let bot_sahip = process.env.SAHIP;
+const moment = require("moment");
+const os = require("os");
+require("moment-duration-format");
+
+exports.run = async (client, message, args) => {
+    .addField("**Ram kullanımı**",true)
+}

268714747efedfe3ec167c3cb4eb5a8b927ed2d2
Utku Ömer Kılıç <omerleroyunda@gmail.com>
2021-01-20 11:45:35 +0300

    Update komut1.js

--- komutlar/komut1.js
+++ komutlar/komut1.js
@@ -6,12 +6,29 @@ const os = require("os");
 require("moment-duration-format");
 
 exports.run = async (client, message, args) => {
-    .addField("**Ram kullanımı**",true)
+  const msg = new Discord.MessageEmbed()
+    .setColor("RANDOM")
+    .setTimestamp()
+    .setFooter(`${message.author.username} Tarafından istendi.`,message.author.avatarURL())
+    .setAuthor(`${client.user.username} - İstatistik:`)
+    .addField("**Ram kullanımı**",`\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\``, true)
+    .addField("**Çalışma süresi**",`\`${moment.duration(client.uptime).format(" D [gün], H [saat], m [dakika], s [saniye]")}\``,true)
+    .addField("**Kullanıcılar**",`\`${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}\``,true)
+    .addField("**Sunucular**",`\`${client.guilds.cache.size.toLocaleString()}\``,true)
+    .addField("**Kanallar**",`\`${client.channels.cache.size.toLocaleString()}\``,true)
+    .addField("**Discord.JS**", `\`v${Discord.version}\``, true)
+    .addField("**Bot sürümü**",`\`v${bot_version}\``,  true)
+    .addField("**Ping**", `\`${client.ws.ping}ms\`` , true)
+    .addField("**Sahip**", `\`${bot_sahip}\``, true);
+  return message.channel.send(msg);
 };
 
 exports.help = {
-    name: "komut1",
-    guildOnly: false, // false olur ise DM'den de kullanılabilir bi hal alır | true olur ise sadece sunucuda kullanılabilir bir halde olur
-    aliases: ["i"]
-  };
-  
+  name: "komut1",
+  guildOnly: false, // false olur ise DM'den de kullanılabilir bi hal alır | true olur ise sadece sunucuda kullanılabilir bir halde olur
+  aliases: ["i"]
+};
+
+
+
+
